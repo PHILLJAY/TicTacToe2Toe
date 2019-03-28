f682a9c7455addfe103d3c18aaf00edb982dc9fe
PHILLJAY <47534916+PHILLJAY@users.noreply.github.com>
2019-03-28 14:08:08 -0400

    1v1 is doable, added option for pvp

--- tictactest.java
+++ tictactest.java
@@ -4,40 +4,58 @@ public class tictactest {
 
 	public static void main(String args[] ) {
 		TicTacToeGame test = new TicTacToeGame();
-		Scanner in = new Scanner(System.in);
-		int pos = 0;
-		System.out.println("hello, welcome to my game of tic tac toe");
-		System.out.println("Would you like to play with a computer? (1/0)");
-		if(in.nextInt()==0) {
-			test.pvp(true);
+		Scanner in =  new Scanner(System.in);
+		System.out.println("welcome to tictactoe!");
+		System.out.println("would you like pvp to be enabled? 1/0");
+		int a = in.nextInt();
+		if(a==1){
+			test.multiplayer(true);
+		}else if(a==0){
+			test.multiplayer(false);
+			System.err.println("ERROR: No pve yet implimented");
+		}else{
+			System.out.println("Alright thanks for picking a number");
+			System.out.println("Ill just set you up against a human, you have friends right?");
+			test.multiplayer(true);
 		}
-		for (int i = 0; i < 50; ++i) System.out.println();
-		System.out.println("Alright, setting up board");
+		int move = 0;
+		System.out.println("Setting up board");
 		test.set();
 		test.display();
-		System.out.println("Alright just type a number in to place there");
-		for(int z = 0; z<9; z++) {
-			System.out.println("Pop a new number in babeee");
-			test.whatTurn();
-			pos = in.nextInt();
-			if(test.isValid(pos)) {
-				test.place(pos);
-				test.turnSwitch();
-				test.xoSwitch();
-			}
-			if(test.isWon()) {
+		System.out.println("looking good :)");
+		for(int z = 0; z<9; z++){
+			test.display();
+			if (test.isWon()){
 				break;
 			}
-			for (int i = 0; i < 50; ++i) System.out.println();
-			if(!test.isValid(pos)){
-				System.out.println("Invalid move");
+			test.isTurn();
+			if(test.whatTurn()){
+				System.out.println("Alrght, please input your next move");
+				move = in.nextInt();
+				if(test.isValid(move)){
+					test.place(move);
+					test.turnSwitch();
+				}else{
+					System.out.println("Sorry that turn is invalid");
+				}
+			}else{
+				if(test.isPvp()){
+					System.out.println("Alrght, please input your next move");
+					move = in.nextInt();
+					if(test.isValid(move)){
+						test.place(move);
+						test.turnSwitch();
+					}else{
+						System.out.println("Sorry that turn is invalid");
+					}
+
+				}else{
+					System.out.println("a l r i g h t, i m m a  g o  h e r e");
+					test.cpuPlace();
+				}
+
 			}
-			test.display();
 		}
-		System.out.println("ALRIGHT THE GAMES DONE");
-		if(test.isWon()) {
-			System.out.println("YOU WIN");
-		}else
-			System.out.println("TIE TIE TIE");
+		System.out.println("someone won");
 	}
 }
